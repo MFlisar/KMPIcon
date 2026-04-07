7c86170cc0de3edc4b0280d74f9e7094c8e92aa5
MFlisar <mflisar.development@gmail.com>
2026-04-07 11:50:40 +0200

    ...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,7 +38,7 @@ pluginManagement {
 
 plugins {
     // version catalogue does not work here!
-    id("io.github.mflisar.kmpdevtools.plugins-settings-gradle") version "7.6.1"
+    id("io.github.mflisar.kmpdevtools.plugins-settings-gradle") version "7.8.0"
 }
 
 val settingsPlugin = plugins.getPlugin(com.michaelflisar.kmpdevtools.SettingsFilePlugin::class.java)
